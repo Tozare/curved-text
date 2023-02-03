1904bd8b03b2880664f71724b27f276326a1712e
Emir Kuanyshev <emir.kuanyshev@nu.edu.kz>
2023-02-03 17:41:18 +0600

    flip bug fixed

--- src/shared/libs/data-path/get-circle-data-path.ts
+++ src/shared/libs/data-path/get-circle-data-path.ts
@@ -9,7 +9,7 @@ export const getCircleDataPath = ({ x, y, r, deg, isTopArcStart }: GetCircleData
   const a = (180 + (isTopArcStart ? deg : -deg))*Math.PI/180;
   const deltaX = Math.abs(r)*Math.cos(a);
   const deltaY = -Math.abs(r)*Math.sin(a);
-  const flip = r ? 1 : 0;
+  const flip = isTopArcStart ? 1 : 0;
   return `
     M ${r} ${r}
     m ${deltaX} ${deltaY}
